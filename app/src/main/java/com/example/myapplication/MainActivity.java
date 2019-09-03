9c5b6e8d20f60863196527c74319ee051cd08729
Wushenhao <646316281@qq.com>
2019-09-03 09:41:52 +0800

    1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,13 +8,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView textViewHelloWorld;//定义了一个组件
+    private TextView textViewHelloWorld;//定义了一个TextView组件
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewHelloWorld=findViewById(R.id.text_view_hello_world);
-        textViewHelloWorld.setText("你好！你好！");
+        textViewHelloWorld=findViewById(R.id.text_view_hello_world); //在actibity_main中通过@+id在R.java文件中自动生成了int型变量text_view_hello_world
+        textViewHelloWorld.setText("你好Android！");
     }
 }
