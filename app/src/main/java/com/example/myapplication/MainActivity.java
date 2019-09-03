ec7078e87e4bbffdeeefcdd729b772788d8b7695
Wushenhao <646316281@qq.com>
2019-09-03 16:15:21 +0800

    找不到符号类，先保存后修改

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,13 +8,25 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private TextView textViewHelloWorld;//定义了一个TextView组件
+    private TextView textViewHelloWorld; //定义TextView控件
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //通过id找到TextView控件对象
         textViewHelloWorld=findViewById(R.id.text_view_hello_world); //在actibity_main中通过@+id在R.java文件中自动生成了int型变量text_view_hello_world
         textViewHelloWorld.setText("你好Android！");
+
+        //通过资源id获得字符串
+        textViewHelloWorld.setText(this.getString(R.string.hello_world));
+
+        //通过资源名获得字符串，如何通过"hello_world"获得R.string.hello_world
+        Context context=this.getApplicationContext();
+        String resName="hello_world";
+        int helloWorldId=context.getResources().getIdentifier(resName,"string",context.getPackageName());
+        textViewHelloWorld.setText(this.getString(helloWorldId));
+
+
     }
 }
